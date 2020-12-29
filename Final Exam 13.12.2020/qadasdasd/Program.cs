9221f258781c944a063194e316d08f73b150ca29
Yulian <iulian.valkanov@gmail.com>
2020-12-29 17:51:46 +0200

    hg

---
+++ Final Exam 13.12.2020/qadasdasd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace qadasdasd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue = true;
+
+            int value = int.Parse(isTrue);
+
+            Console.WriteLine(value);
+
+        }
+
+        private static void Readname()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
