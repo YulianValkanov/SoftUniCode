b920d73054e2d5d6e54b86815f1987ac2427654f
YulianValkanov <75361120+YulianValkanov@users.noreply.github.com>
2022-04-02 14:06:23 +0300

    Add files via upload

---
+++ Databases Advanced - Entity Framework cour/1. ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first=Console.ReadLine();
+
+            while (first!="stop")
+            {
+                if (first == "Hi")
+                {
+                    Console.WriteLine("Yeas");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+
+                 first = Console.ReadLine();
+            }
+           
+        }
+    }
+}
