71f6e3a0ca16dc48d511d2733b55b6fc94fceaad
YulianValkanov <75361120+YulianValkanov@users.noreply.github.com>
2022-04-03 19:29:14 +0300

    Add files via upload

---
+++ Databases Advanced - Entity Framework cour/Databases Advanced - Entity Framework cour/2. AdoNet/AdoNet/Configurations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoNet
+{
+    public static class Configurations
+    {
+
+        public const string CONNECTIONS_STRING = @"Server=DESKTOP-8164Q69;Database=MinionsDB;Trusted_Connection=True;";
+    }
+}
